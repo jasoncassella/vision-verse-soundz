6548975a973818c9a94789969131100da247030f
Jason Cassella <jasoncassella1@gmail.com>
2022-12-25 15:29:59 -0500

    working newsletter signup

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,17 @@ const Footer = () => {
 	const year = d.getFullYear();
 	return (
 		<footer>
+			<form
+				action='https://getform.io/f/aae45edf-47a7-4f08-bd02-d86520a69b92'
+				method='POST'>
+				<div className='container'>
+					<input type='text' placeholder='phone' name='phone' id='phone' required />
+					<input type='text' placeholder='email' name='email' id='email' />
+					<div>
+						<button type='submit'>Subscribe</button>
+					</div>
+				</div>
+			</form>
 			<small>&copy; {year} Vision Verse Soundz</small>{' '}
 		</footer>
 	);
