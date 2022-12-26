34c94a185ebcffdbf49c7f7140d7645d30d0b9b3
Jason Cassella <jasoncassella1@gmail.com>
2022-12-26 09:43:47 -0500

    * to indicate this field is required

--- components/Footer.js
+++ components/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
 				action='https://getform.io/f/aae45edf-47a7-4f08-bd02-d86520a69b92'
 				method='POST'>
 				<div className='container'>
-					<input type='text' placeholder='phone' name='phone' id='phone' required />
-					<input type='text' placeholder='email' name='email' id='email' />
+					<input type='text' placeholder='Phone *' name='phone' id='phone' required />
+					<input type='text' placeholder='Email' name='email' id='email' />
 					<div>
 						<button type='submit'>Subscribe</button>
 					</div>
