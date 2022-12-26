7ff5bdd5e67de476b8b51259c8e51dafb15f4ca2
Jason Cassella <jasoncassella1@gmail.com>
2022-12-26 13:27:21 -0500

    basic hover effect on the albums

--- components/Album.js
+++ components/Album.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Album = props => {
 	return (
-		<div className='ablum'>
+		<div className='album'>
 			<Link href={props.url} target="_blank">
 				<Image src={props.imgUrl} alt={props.name} width={300} height={300} />
 			</Link>
