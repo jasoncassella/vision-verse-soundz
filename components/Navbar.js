411abbbf8cf2f065f8cd272adb15e491b547222f
Jason Cassella <jasoncassella1@gmail.com>
2022-12-26 11:37:47 -0500

    formatting

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,11 +5,17 @@ const Navbar = () => {
 	return (
 		<nav>
 			<Link className='link' href='/' id='logo'>
-				<Image src="/logo.png" alt="logo" width={100} height={64}  />
+				<Image src='/logo.png' alt='logo' width={100} height={64} />
 			</Link>
-			<Link className='link' href='/artists' >Artists</Link>
-			<Link className='link' href='/releases' >Releases</Link>
-			<Link className='link' href='/contact' >Contact</Link>
+			<Link className='link' href='/artists'>
+				Artists
+			</Link>
+			<Link className='link' href='/releases'>
+				Releases
+			</Link>
+			<Link className='link' href='/contact'>
+				Contact
+			</Link>
 		</nav>
 	);
 };
